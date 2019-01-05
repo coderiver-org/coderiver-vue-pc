3993babcbc78da2407dd31caf482924346f355cb
@gi-fe-lsm <@gi-fe>
2019-01-05 18:43:37 +0800

    api目录结构

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+// iview
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+Vue.use(iView);
+
+// api
+import API from './api/api';
+Vue.prototype.$api = new API('/webapi');
+
 new Vue({
   router,
   store,
