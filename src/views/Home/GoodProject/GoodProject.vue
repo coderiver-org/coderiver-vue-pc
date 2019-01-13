ebc453186e6c1bcabb9827fa56a9d8b5cdaccae0
jiantinglian <jiantinglian@shanghaizhuandeMacBook-Pro.local>
2019-01-13 15:23:45 +0800

    goodproject init

---
+++ src/views/Home/GoodProject/GoodProject.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="wrapper">
+    <div class="line"></div>
+    <div class="info_wrapper">
+      <span class="info_detail">优秀项目推荐</span>
+      <router-link to="/project-center" tag='a' class="readmore">查看更多</router-link>
+    </div>
+    <good-project-item :goodProjectItem='goodProjectItem'></good-project-item>
+  </div>
+</template>
+<script>
+import GoodProjectItem from './GoodProjectItem'
+export default {
+  data(){
+    return {
+      goodProjectItem:[
+        {
+          id:1,
+          imgUrl:'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1547264904&di=091ae12eadb879b1add718b282af086f&src=http://s3.sinaimg.cn/middle/6f32f292g94155f2a2202&690',
+          projectName:'今天天气不错今天天气不错今天天气不错',
+          desc:'今天天气不错今天天气不错今天天气不错今天天气不错今天天气不错今天天气不错',
+          see:'1234',
+          good:'123'
+        },{
+          id:2,
+          imgUrl:'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1547264904&di=091ae12eadb879b1add718b282af086f&src=http://s3.sinaimg.cn/middle/6f32f292g94155f2a2202&690',
+          projectName:'今天天气不错今天天气不错今天天气不错',
+          desc:'今天天气不错今天天气不错今天天气不错今天天气不错今天天气不错今天天气不错',
+          see:'1234',
+          good:'123'
+        },{
+          id:3,
+          imgUrl:'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1547264904&di=091ae12eadb879b1add718b282af086f&src=http://s3.sinaimg.cn/middle/6f32f292g94155f2a2202&690',
+          projectName:'今天天气不错今天天气不错今天天气不错',
+          desc:'今天天气不错今天天气不错今天天气不错今天天气不错今天天气不错今天天气不错',
+          see:'1234',
+          good:'123'
+        }
+      ]
+    }
+  },
+  components:{
+    GoodProjectItem
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper{
+  margin: 0 auto;
+  width: 66.67%;
+  .line{
+    width: 95%;
+    height:4px;
+    background:rgba(64,174,168,1);
+    margin: 0 auto;
+    margin-bottom: 48px;
+  }
+  .info_wrapper{
+    margin-bottom:70px;
+    font-size:333%;
+    font-family:Adobe Heiti Std R;
+    font-weight:normal;
+    color:rgba(24,38,42,1);
+    line-height:26px;
+    .info_detail{
+      font-size:100%;
+      font-family:Adobe Heiti Std R;
+      font-weight:normal;
+      color:rgba(24,38,42,1);
+      line-height:26px;
+    }
+    .readmore{
+      float: right;
+      font-size:75%;
+      font-family:Adobe Heiti Std R;
+      font-weight:normal;
+      color:rgba(64,174,168,1);
+      line-height:26px;
+      :hover{
+        cursor: pointer;
+      }
+    }  
+  } 
+}
+</style>
---
+++ src/views/Home/GoodProject/GoodProjectItem.vue
@@ -0,0 +1,88 @@
+<template>
+<Row>
+  <i-col :span='12' class="item_wrapper" v-for="item in goodProjectItem" :key='item.id'>
+    <div class="img_wrapper">
+      <img :src="item.imgUrl" class='item_img' alt="">
+    </div>
+    <p class="project_name">{{item.projectName}}</p>
+    <p class="project_info">{{item.desc}}</p>
+    
+    <div style="height:24px;line-height:24px;font-size:20px;color:#999;">
+      <div class="icon_wrapper">
+        <img class="see_icon" src="@/assets/see_icon@2x.png" alt="">
+        <span>{{item.see}}</span>
+      </div>
+      <div class="icon_wrapper">
+        <img class="zan_icon" src="@/assets/zan_icon@2x.png" alt="">
+        <span ref="count">{{item.good}}</span>
+      </div>
+      
+    </div>
+  </i-col>
+</Row>
+</template>
+<script>
+export default {
+  name: 'GoodProjectItem',
+  props:{
+    goodProjectItem: Array
+  }
+}
+</script>
+<style lang="scss" scoped>
+.item_wrapper{
+  padding:5.2%;
+  margin-bottom:30px; 
+  &:hover{
+    box-shadow:0px 4px 5px 1px rgba(0, 0, 0, 0.3);
+    z-index: 2;
+  }
+  .img_wrapper{
+    height: 0;
+    padding-bottom:66.9%;
+    overflow: hidden;  
+    .item_img{
+      width:100%;
+    }
+  }
+  .project_name{
+    font-size:250%;
+    font-family:Adobe Heiti Std R;
+    font-weight:normal;
+    color:rgba(24,38,42,1);
+    line-height:26px;
+    margin-bottom:14px;
+    overflow: hidden;/*超出部分隐藏*/
+    white-space: nowrap;/*不换行*/
+    text-overflow:ellipsis;/*超出部分文字以...显示*/
+  }
+  .project_info{
+    font-size:167%;
+    font-family:Adobe Heiti Std R;
+    font-weight:normal;
+    color:rgba(153,153,153,1);
+    line-height:130%;
+    margin-bottom:30px;
+    overflow: hidden;/*超出部分隐藏*/
+    white-space: nowrap;/*不换行*/
+    text-overflow:ellipsis;/*超出部分文字以...显示*/
+  }
+  .icon_wrapper{
+    width:40%;
+    display: inline-block;
+    margin-right:5%;
+    .see_icon{
+      width:30px;
+      height: 18px;
+      margin-right:5%;
+    }
+    .zan_icon{
+      width:24px;
+      height:24px;
+      cursor: pointer;
+      margin-right:5%;
+    }
+  }
+  
+}
+</style>
