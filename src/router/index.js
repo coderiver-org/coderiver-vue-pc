8b9dbbd36d51c82c45cd0406d0202ff3be8f889f
wlzs <wlzs93@163.com>
2019-01-14 21:58:16 +0800

    注册页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import Layout from '@/views/Layout/main.vue'
-
 Vue.use(Router)
 
 export default new Router({
@@ -37,6 +36,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ '@/views/Login.vue')
+    },
+    {
+      path:'/register',
+      name:'register' ,
+      component: () => import('@/views/Register/register')
     },
     {
       path: '*',
--- src/views/Layout/main.vue
+++ src/views/Layout/main.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wapper">
-    <img alt="Vue logo" src="@/assets/logo.png">
+    <img alt="Vue logo" src="@/assets/icons/logo.png">
     <div id="nav">
       <router-link to="/">首页</router-link> |
       <router-link to="/project-center">项目中心</router-link> |
